09928139f961acd856258a75a135b348f5a90473
Zoe <33168975+ZoeLeee@users.noreply.github.com>
2018-08-05 22:34:20 +0800

    微信小程序布局

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,13 +2,29 @@
 //获取应用实例
 const app = getApp()
 
+const amusement=[
+  {title:"音乐",url:"",icon:"icon-customerservice_fill"},
+  {title:"相声评书",url:"",icon:"icon-customerservice_fill"},
+  {title:"段子",url:"",icon:"icon-customerservice_fill"},
+  {title:"情感生活",url:"",icon:"icon-customerservice_fill"},
+  {title:"音乐",url:"",icon:"icon-customerservice_fill"},
+  {title:"音乐",url:"",icon:"icon-customerservice_fill"},
+  {title:"音乐",url:"",icon:"icon-customerservice_fill"},
+  {title:"音乐",url:"",icon:"icon-customerservice_fill"},
+  {title:"音乐",url:"",icon:"icon-customerservice_fill"},
+]
+
 Page({
   data: {
-    motto: 'Hello World',
-    index:0,
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    historys:[],
+    bookCategory:[
+      {title:"有声小说",url:"",icon:"icon-customerservice_fill",category:[{title:"言情",url:""},{title:"悬疑",url:""},{title:"都市",url:""}]},
+      {title:"畅销书",url:"",icon:"icon-tushu",category:[{title:"言情",url:""},{title:"悬疑",url:""},{title:"都市",url:""}]},
+      {title:"儿童",url:"",icon:"icon-wangwang",category:[{title:"言情",url:""},{title:"悬疑",url:""},{title:"都市",url:""}]}],
+    amusement,
   },
   onLoad: function () {
     
